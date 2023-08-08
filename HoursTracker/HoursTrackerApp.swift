4a4fc1a437340f57ee0d666e38c37fa001a2f68b
Jack Finnis <jack.finnis@icloud.com>
2023-08-08 19:24:46 +0100

    Initial Commit

---
+++ HoursTracker/HoursTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoursTrackerApp.swift
+//  HoursTracker
+//
+//  Created by Jack Finnis on 08/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct HoursTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
